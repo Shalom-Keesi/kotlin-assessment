b7db3f5e360cf662f98efb9996573cd0f9e7aa6f
Shalom-Keesi <shalomkeesi@gmail.com>
2024-03-25 08:33:19 +0300

    added files

---
+++ src/Main.kt
@@ -0,0 +1,43 @@
+import kotlin.time.times
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//    println(listOf({statement}))
+//    sentence(statement="")
+
+    var array= arrayOf(10,20,30,40)
+    println(array.sum())
+    println(array.count())
+    println(array.sum()/array.count())
+
+
+}
+
+fun sentence(statement:String){
+    var statement="Barnie bakes brown bagels and buns"
+
+
+}
+fun arr1(array: Array<Int>):Int{
+
+    return array.sum()
+    return array.count()
+    return array.sum()/array.count()
+}
+
+fun sphere(){
+    var radius="r"
+    var volume=4/3 * 3.14159
+    println(volume)
+}
+
+fun isPalindrome(word:String):Boolean{
+    if word==word.reversed()
+    println(true)
+    else{
+        println(false)
+    }
+}
+
+
